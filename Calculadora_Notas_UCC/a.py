5d411e4f300bc34651cd5de0830bf541fdf4835c
Felipe Castro <118694429+MarkZ1953@users.noreply.github.com>
2024-06-11 12:44:28 -0500

    Update

---
+++ Calculadora_Notas_UCC/a.py
@@ -0,0 +1,2 @@
+for k, v in {'a': {'creditos': 3, 'calificacion': 3.0, 'nota': 9.0}, 'b': {'creditos': 3, 'calificacion': 5.0, 'nota': 15.0}}.items():
+    print(v["creditos"])
--- Calculadora_Notas_UCC/calculadora.py
+++ Calculadora_Notas_UCC/calculadora.py
@@ -1,17 +1,72 @@
 from texttable import Texttable
 
-table = Texttable()
+# table = Texttable()
 
 
 class CalculadoraNotasUCC:
-    pass
+    def __init__(self) -> None:
+        self.notas = {}
+        self.ejecutarCalculadora()
+        
+    def ejecutarCalculadora(self):
+        while True:
+            self.mostrarMenu()
+            opcionMenu = int(input("Ingrese una opcion: "))
+            
+            if opcionMenu == 1:
+                self.calcularNotas()
+            if opcionMenu == 5:
+                break 
 
+    def calcularNotas(self):
+        cantidadMaterias = int(input("Ingrese la cantidad de materias: "))
+        promedio: float = 0
+        sumaCreditos: int = 0
+        sumaNotas: float = 0
 
-table.add_rows([
-    ["Name", "Age", "City"],
-    ["Alice", 30, "New York"],
-    ["Bob", 25, "Los Angeles"],
-    ["Charlie", 35, "Chicago"]
-])
+        for i in range(cantidadMaterias):
+            nombreMateria = input("Ingrese el nombre de la materia: ")
+            creditosMateria = int(input("Ingrese los creditos de la materia: "))
+            calificacionMateria = float(input("Ingrese la calificacion de la materia: "))
+            notaMateria = int(creditosMateria) * float(calificacionMateria)
 
-print(table.draw())
+            sumaCreditos += creditosMateria
+            sumaNotas += notaMateria
+
+            self.notas[nombreMateria] = {
+                "creditos": creditosMateria,
+                "calificacion": calificacionMateria,
+                "nota": notaMateria
+            }
+
+        promedio = sumaNotas / sumaCreditos
+
+        self.mostrarTablaCalificaciones(self.notas)
+        
+        print(f"Su promedio es de: {round(promedio, 3)}")
+    
+    def mostrarTablaCalificaciones(self, notas):
+        tablaCalificaciones = Texttable()
+        tablaCalificaciones.add_row(["Nombre", "Creditos", "Calificacion", "Nota"])
+
+        print(notas)
+        
+        for nombreMateria, detalles in notas.items():
+            tablaCalificaciones.add_row([nombreMateria, detalles["creditos"], detalles["calificacion"], detalles["nota"]])
+
+        print(tablaCalificaciones.draw())
+
+    def guardarNotas(self):
+        pass
+
+    def actualizarNota(self):
+        pass
+
+    def mostrarMenu(self):
+        print("Calculadora Notas UCC".center(50, "="))
+        print("1. Calcular notas")
+        print("2. Calcular promedio ponderado")
+        print("3. Guardar notas")
+        print("4. ")
+        print("5. Salir")
+        print("".center(50, "="))
